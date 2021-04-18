292df6ef7f075b3f3ae75c2e5d23cf03ec3cc689
Paola Ruiloba <75644688+PaoPao27402@users.noreply.github.com>
2021-04-18 21:34:03 +0300

    update

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 function App() {
   return (
+    <div className="container">  
     <div className="App">
       <header className="App-header">
         < Weather city ="Tokyo"/>
       </header>
+    </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -11,8 +11,6 @@ export default function Weather(props){
  
     }
 
-
-
     let apiKey = "094780c710fa4efd669f0df8c3991927";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
