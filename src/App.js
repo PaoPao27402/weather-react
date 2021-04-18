aa39c6afd0d3fd670d56e5bfc52c4ad336947b5a
Paola Ruiloba <75644688+PaoPao27402@users.noreply.github.com>
2021-04-18 23:01:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Weather from "./Weather";
-import Search from "./Search";
 import './App.css';
 
 
@@ -10,7 +9,6 @@ function App() {
       <div className="wrapper">
         < Weather city ="Tokyo"/>
       </div>
-      <Search />
     </div>
   );
 }
